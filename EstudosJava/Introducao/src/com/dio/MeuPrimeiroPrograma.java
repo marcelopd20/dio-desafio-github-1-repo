39cf19bf5f67c00baba5a0fa00862872dce6d64a
marcelopd20 <marcelopd20@gmail.com>
2022-02-13 11:32:08 -0300

    Hello World

---
+++ EstudosJava/Introducao/src/com/dio/MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package com.dio;//define pacote da classe
+
+public class MeuPrimeiroPrograma { //criaaclasse
+    //criaometodo(publico com array de strings)
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
